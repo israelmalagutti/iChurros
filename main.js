a65f4c8cf22d83f958b32b0be4b4abb3ceccf4f2
Israel Malagutti <israelmalagutti@gmail.com>
2022-02-16 17:07:38 -0300

    Squashed commit of the following:
    commit eb061c77101a0aac059a3fe0b02c7a134d2e3bcc
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Wed Feb 16 17:06:51 2022 -0300
    
        Update main.js
    
    commit e10775c7e324bbe61efb26ef2967e1beead1c15f
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Wed Feb 16 16:55:28 2022 -0300
    
        FUNCIONOU DE VEZ 3
    
    commit 01d87947238399b27cf5dfd6db6ed822ed841ea1
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Wed Feb 16 12:16:04 2022 -0300
    
        FUNCIONOU
    
        FUNCIONOU
    
        Funcionou mas em determinadas condições ainda dá erro
    
    commit 030f1039627e82b87f73be51db3345e7379cf2a3
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Wed Feb 16 10:23:07 2022 -0300
    
        Update JS Validations 2
    
    commit 54b2cbf8b8d4914ca96dfabd60c8e01da59a1632
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Tue Feb 15 17:49:15 2022 -0300
    
        Update JS Validations
    
    commit 234100ba9173821947e32c21beeaaaf94c3cbeb9
    Author: Israel Malagutti <israelmalagutti@gmail.com>
    Date:   Tue Feb 15 16:36:41 2022 -0300
    
        Js validation

---
+++ main.js
@@ -0,0 +1,68 @@
+const form = document.getElementById("form")
+let error = document.getElementById("error")
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+    document.getElementById("error").innerHTML = "";
+
+    function emailValidation() {
+        const email = document.getElementById("email").value;
+        if (email == "") {
+            document.getElementById("error").innerHTML = "Email é obrigatório!";
+            return false;
+        }
+        return true;
+    }
+        
+    // Validação da Senha
+    function passwordValidation() {  
+        const password = document.getElementById("password").value;  
+        // Checkar se o campo está vazio  
+        if(password == "") {  
+            document.getElementById("error").innerHTML = "A senha precisa ser preenchida!";
+            return false;  
+        }
+        
+        if(password.length < 8) {   
+            document.getElementById("error").innerHTML = "Este campo precisa ter mais do que 8 caracteres";
+            return false;
+        }
+        return true;
+    }
+    
+    //Checar se as senhas são iguais
+    function passwordMatch() {
+        const password = document.getElementById("password").value; 
+        const confirmPassword = document.getElementById("confirmPassword").value;
+        
+        if( confirmPassword !== password) {
+            document.getElementById("error").innerHTML = "As senhas devem ser iguais!";
+            return false;
+        }
+        return true;
+    }
+        
+    // Aviso Checkbox Desmarcada
+    function checkboxValidation() {
+        let check = document.getElementById("checkbox").checked
+        if (check == false) {
+            document.getElementById("error").innerHTML = "A checkbox deve estar marcada!";
+            return false;
+        } 
+        return true;
+    }
+
+    // Em caso de envio bem-sucedido do formulário, a página de confirmação deve ser exibida
+    function switchPage() {
+        // window.location.href();
+        if (error.innerHTML === "") {
+            location.assign("./confirmation.html")
+        }
+    }
+    
+    if (!emailValidation()) return;
+    if (!passwordValidation()) return;
+    if (!passwordMatch()) return;
+    if (!checkboxValidation()) return;
+    switchPage();
+})
